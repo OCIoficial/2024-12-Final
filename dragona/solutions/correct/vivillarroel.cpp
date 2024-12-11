5cd68dff1be5333848fe31d5459564eb65da3ba6
Nico Lehmann <nico.lehmannm@gmail.com>
2024-12-11 12:31:24 -0300

    Dragona: solución en java + arreglar solución vi

--- dragona/solutions/correct/vivillarroel.cpp
+++ dragona/solutions/correct/vivillarroel.cpp
@@ -1,15 +1,3 @@
-// This file is a partial solution for the sample task. You can add partial
-// solutions just by placing them in this directory. Remove this file before
-// you start working on the actual task.
-//
-// Partial solutions must specify the list of subtasks they should fail with a
-// comment as exemplified below. Ocimatic will check the solution fails these
-// subtasks and only these subtasks. If no comment is present, ocimatic will
-// assume all subtasks should fail.
-//
-// For the sample task, this solution should fail the subtask 2 because it uses
-// `int` instead of `long`. This implies the solution should pass subtask 1.
-
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -24,7 +12,7 @@ int main() {
 		cin >> a >> b;
 		adj[a].push_back(b);
 	}
-	vector<int> ans(h+1,100000);
+	vector<int> ans(h+1,1000000);
 	ans[1] = 0;
 	for (int i = 1; i < h; ++i) {
 		for (auto x : adj[i]) {
